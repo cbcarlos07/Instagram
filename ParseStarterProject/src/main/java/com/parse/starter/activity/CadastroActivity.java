35932d0eefa278ff84cfd3329c22b9d3bd6e0fa2
Carlos Bruno <cbcarlos08@gmail.com>
2017-06-28 15:03:07 -0400

    Curso Android - Cadastro Activity

--- ParseStarterProject/src/main/java/com/parse/starter/activity/CadastroActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/activity/CadastroActivity.java
@@ -1,7 +1,9 @@
 package com.parse.starter.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.parse.starter.R;
 
@@ -12,4 +14,12 @@ public class CadastroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
     }
+
+    public void abrirLoginUsuario(View view){
+
+        Intent intent = new Intent( CadastroActivity.this, LoginActivity.class );
+        startActivity( intent );
+
+    }
+
 }
